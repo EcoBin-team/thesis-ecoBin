9995d96817c7ebd7c814f6728ecd908cd5664867
nourane-abderrahim <abderrahim.nourane@gmail.com>
2023-06-08 12:32:08 +0200

    commit to pull

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,8 @@ const app = express();
 const cors = require("cors");
 const supabaseConnect = require("./supabase/Supabase_Connect");
 const userRouter = require("./routes/routerUser")
-<<<<<<< HEAD
-const feedsRouter = require("./routes/routerfeeds")
-=======
 const depotRouter = require("./routes/depotRouter")
 const SearchRouter = require("./routes/searchRouter")
->>>>>>> 84ec412accdb99555283ae69c7c2d96e21fa3de0
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,9 +16,6 @@ app.use((req, res, next) => {
   next();
 });
 app.use("/users", userRouter)
-<<<<<<< HEAD
-app.use("/", feedsRouter)
-=======
 app.use("/", depotRouter)
 app.use("/", SearchRouter)
 
@@ -30,7 +23,6 @@ app.use("/", SearchRouter)
 
 
 
->>>>>>> 84ec412accdb99555283ae69c7c2d96e21fa3de0
 
 app.listen(3000, () => {
   console.log('> Ready on http://localhost:3000');
