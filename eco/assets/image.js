c690dd7dabef75b17104e480c4d6aa2ad3ce6bfe
nourane-abderrahim <abderrahim.nourane@gmail.com>
2023-06-08 10:17:27 +0200

    commit to pull

--- eco/assets/image.js
+++ eco/assets/image.js
@@ -1,8 +1,8 @@
 const Images={
-    img1:require('./img2.png'),
+    img1:require('./img.jpg'),
     img2:require('./img3.jpg'),
     img3:require('./img5.png'),
-    img4:require('./img4.jpg'),
+    img4:require('./nn.jpg'),
     vector:require('./vector.png'),
 }
 export default Images
