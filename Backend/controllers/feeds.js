5c18c9135dbd4acd3a06a47816b261a00d1c5de2
Ines-Triki <inestriki226@gmail.com>
2023-06-11 16:41:38 +0200

    backlikes

--- Backend/controllers/feeds.js
+++ Backend/controllers/feeds.js
@@ -86,6 +86,116 @@ const postComment = async (req, res) => {
 };
 
 
+const postLike = async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.body;
+
+  console.log('Received postLike request with id:', id);
+  console.log('Received postLike request with userId:', userId);
+
+  try {
+    // Check if userId is provided
+    if (!userId) {
+      return res.status(400).json({ error: 'userId is required in the request body' });
+    }
+
+    // Check if userId is a valid non-empty string
+    if (typeof userId !== 'string' || userId.trim() === '') {
+      return res.status(400).json({ error: 'userId must be a non-empty string' });
+    }
+
+    // Create a new like object with the required fields
+    const newLike = {
+      postid: id,
+      userid: userId,
+    };
+
+    console.log('Inserting new like:', newLike);
+
+    // Insert the new like into the database
+    const { data: like, error } = await supabase
+      .from('likes')
+      .insert(newLike)
+      .single();
+
+    if (error) {
+      console.error(error); // Log the error to the console for debugging
+      throw new Error(error.message);
+    }
+
+    console.log('Inserted like:', like);
+
+    res.status(201).json(like);
+  } catch (error) {
+    console.error(error); // Log the error to the console for debugging
+    res.status(500).json({ error: error.message });
+  }
+};
 
 
-module.exports = { getOneFeed, getAllFeeds,postComment,getAllComments };
+
+
+const deleteLike = async (req, res) => {
+  const { postId, userId } = req.params;
+
+  console.log('Received deleteLike request with postId:', postId);
+  console.log('Received deleteLike request with userId:', userId);
+
+  try {
+    // Delete the like from the database
+    const { data: like, error } = await supabase
+      .from('likes')
+      .delete()
+      .match({ postid: postId, userid: userId })
+      .single();
+
+    if (error) {
+      console.error(error); // Log the error to the console for debugging
+      throw new Error(error.message);
+    }
+
+    console.log('Deleted like:', like);
+
+    res.status(200).json(like);
+  } catch (error) {
+    console.error(error); // Log the error to the console for debugging
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getAllLikesByPostId = async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    if (!postId) {
+      throw new Error('Post ID is required');
+    }
+
+    const { data: likes, error } = await supabase
+      .from('likes')
+      .select('*')
+      .eq('postid', postId);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    res.status(200).json(likes || []);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+
+
+
+
+
+module.exports = { getOneFeed, getAllFeeds, postComment, getAllComments, postLike ,deleteLike,getAllLikesByPostId};
+
+
+
+
+
+
