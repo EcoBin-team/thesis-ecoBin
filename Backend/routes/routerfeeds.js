80c966acaae4f7ef12c5f512d4533913e1712fc5
Ines-Triki <inestriki226@gmail.com>
2023-06-15 18:37:34 +0200

    after pull

--- Backend/routes/routerfeeds.js
+++ Backend/routes/routerfeeds.js
@@ -1,4 +1,4 @@
-const { getOneFeed, getAllFeeds,postComment ,getAllComments,postLike,deleteLike,getAllLikesByPostId,updateLikes} = require('../controllers/feeds');
+const { getOneFeed, getAllFeeds,postComment ,getAllComments} = require('../controllers/feeds');
 const express = require('express');
 const router = express.Router();
 // GET one feed
@@ -9,12 +9,4 @@ router.get('/feeds', getAllFeeds);
 // comment
 router.post('/feeds/:id/postComment',postComment);
 router.get('/feeds/:id/comments', getAllComments);
-
-// like
-router.post('/feeds/:id/postLike', postLike);
-router.put('/feeds/:id/likes', updateLikes);
-
-router.delete('/likes/:postId/:userId', deleteLike);
-router.get('/likes/post/:postId', getAllLikesByPostId);
 module.exports = router;
-
