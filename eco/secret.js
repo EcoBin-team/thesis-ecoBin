392ebbeb8737954c8c469f1496690452d4870396
RayenTellissy <rayentellissy571@gmail.com>
2023-06-18 22:03:19 +0100

    .

--- eco/secret.js
+++
@@ -1,9 +0,0 @@
-export const apiKey = "AIzaSyAFz9nreaxasIXUN88ZbaGzMk2fKzHBYMU"
-export const authDomain = "ecobin-d3109.firebaseapp.com"
-export const projectId = "ecobin-d3109"
-export const storageBucket = "ecobin-d3109.appspot.com"
-export const messagingSenderId = "810001797869"
-export const appId = "1:810001797869:web:72063a93ccd7ecbca172e4"
-
-export const server_url = "https://ecobin.onrender.com"
-export const socket_url = "https://ecobin-socket-server.onrender.com"
