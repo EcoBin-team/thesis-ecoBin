514e0994f44a33f28e88f60b0521c2155d7a63a4
RayenTellissy <rayentellissy571@gmail.com>
2023-06-20 15:15:07 +0100

    fixed bugs

--- eco/components/AuthButton/AuthButton.jsx
+++ eco/components/AuthButton/AuthButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native"
 
-const AuthButton = ({text,fn,style}) => {
+const AuthButton = ({ text, fn, style }) => {
 
   return (
     <TouchableOpacity style={{backgroundColor: "#09E4AF",display: "flex", alignItems: "center", justifyContent: "center",...style}} onPress={fn}>
